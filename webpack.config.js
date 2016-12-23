47a03a2ba0746c73ea280bacd56eb0d775b6cbf3
njscococo <njscococo@gmail.com>
2016-12-23 15:55:50 +0800

    加入mobx及移掉redux

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,10 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['es2015', 'react'],
+        presets: ['es2015', 'react','stage-1'],
+        plugins: [
+          'transform-decorators-legacy',
+        ]
       },
     }],
   },
